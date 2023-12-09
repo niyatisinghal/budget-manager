0f9202f93031b152af7ae3860ced8b657fb26403
Niyati Singhal <niyatisinghal2334@gmail.com>
2023-12-09 19:45:50 +0530

    committing small changes

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def delete_expense(id):
     return redirect(url_for('expenses'))
 
 
-if __name__ == '__main_':
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
